5c5ca0ac9d357416d66de590260397a60665b592
tmdcks2175 <tmdcks2175@konkuk.ac.kr>
2023-04-13 23:40:40 +0900

    tasklist등 수정

--- include/fileio.h
+++ include/fileio.h
@@ -1,9 +1,6 @@
-﻿#define MAX_LINE 1000
-
-#include <task.h>
+﻿#include <task.h>
 
 void fileDiagnosis(); //무결성검사 실패시 return -1
 Task* readFile();//Task 동적으로 저장
 void enrollTask(Task);//파일 추가
 void deleteTask(const char*);//파일 지우기
-int TaskCount();// Task 갯수 인트 반환
