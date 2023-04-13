f39a275b3a5daa7162d968239ba770b3967b5219
tmdcks2175 <tmdcks2175@konkuk.ac.kr>
2023-04-13 23:43:52 +0900

    겹친파일 수정

--- include/fileio.h
+++ include/fileio.h
@@ -1,13 +1,6 @@
 ﻿#include <task.h>
 
-<<<<<<< HEAD
-void fileDiagnosis(); //무결성검사 실패시 return -1
-Task* readFile();//Task 동적으로 저장
-void enrollTask(Task);//파일 추가
-void deleteTask(const char*);//파일 지우기
-=======
 int fileDiagnosis();
 TaskList readFile();
 void enrollTask(Task);
 void deleteTask(const char *);
->>>>>>> 76804c4717eaea4f5e76091816490b0e90e64ab1
