3063c35ca888b5475618f0ee75b109068ec1a86b
S1MPLKH <129380284+S1MPLKH@users.noreply.github.com>
2023-04-15 17:17:19 +0900

    Update userio.c

--- module/userio.c
+++ module/userio.c
@@ -1,18 +1,50 @@
 #include <userio.h>
 
 // 프롬포트에서 입력받은 내용을 ipt에 저장
-void getUsrIpt(const char * ipt){
-
+void getUsrIpt(const char* ipt) {
+	scanf("%s", ipt);
 }
 
 // msg를 terminal에 출력
-void printSysMsg(const char * msg){
-
+void printSysMsg(const char* msg) {
+	printf("%s\n", msg);
 };
 
 // 문자 유형을 구분 (개행 문자 때문에 배열을 입력으로 받음)
-Chr chrType(const char * chr){
-  return 1;
+Chr chrType(const char* chr) {
+    char c = chr[0];
+    if (c >= '0' && c <= '9') {
+        return NumChr;
+    }
+    else if (c >= 'a' && c <= 'z') {
+        return AlphChr;
+    }
+    else if (c >= 'A' && c <= 'Z') {
+        return AlphChr;
+    }
+    else if (c == ' ') {
+        return SpaceChr;
+    }
+    else if (c == '\n') {
+        return LnBrkChr;
+    }
+    else if (c == '.' || c == '-' || c == '/') {
+        return SpecialChr;
+    }
+    else if (c == '\t') {
+        return SpaceChr;
+    }
+    else if (c == '\r') {
+        return LnBrKChr;
+    }
+    else if (c & 0x80 != 0) {
+        return KorChr;
+    }
+    else {
+        return NotChr;
+    }
+
+	
 }
 
 // 문자열 유형을 구분
