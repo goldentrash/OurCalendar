e9a2007584967ec1d0f2bb322f74ca54792e8b46
white piano <startoflateness@gmail.com>
2023-04-16 15:02:50 +0900

    parse과정에서 터지던 문제 수정

--- module/command.c
+++ module/command.c
@@ -5,6 +5,31 @@
 #include <task.h>
 #include <string.h>
 
+const int SNT_LIMIT = 10;
+
+// 사용자 입력에서 인자를 파싱하고, 그럴 수 없으면 raise exception
+// 예시)
+// parseParameters("delete abc123\r\n", [PureStr, SpaceStr, PureStr, LnBrkStr], parameters);
+// parameters[0]에는 첫 번째 인자가 저장된다.
+int parseParameters(const char *usrIpt, const Str *cmdSyntax, const int syntaxLen, char parameters[SNT_LIMIT][INP_LIMIT])
+{
+    int now = 0;
+    for (int i = 0; i < syntaxLen; i++)
+    {
+        int nxt = stepStr(usrIpt, now, cmdSyntax[i], parameters[i]);
+
+        if (nxt == 0)
+            return -1;
+
+        now += nxt;
+    }
+
+    if (strlen(usrIpt) > now)
+        return -1;
+
+    return 1;
+};
+
 // 문자열을 받아서 명령어 종류 판가름
 Cmd cmdType(const char *str)
 {
@@ -51,7 +76,7 @@ void help(const char *str)
 void add(const char *str)
 {
     const Str syntax[8] = {PureStr, SpaceStr, DateStr, SpaceStr, DateStr, SpaceStr, BooleanStr, LnBrkStr};
-    const char **parameters[8];
+    char parameters[SNT_LIMIT][INP_LIMIT];
 
     if (parseParameters(str, syntax, 8, parameters) == -1)
     {
@@ -82,7 +107,7 @@ void add(const char *str)
         return;
     }
 
-    enrollTask(mkTask(comp, stDate, enDate, contents));
+    enrollTask(mkTask(comp[0], stDate, enDate, contents));
     printSysMsg("일정이 추가되었습니다.");
 };
 
@@ -92,13 +117,13 @@ void del(const char *str)
     const Str syntax1[4] = {PureStr, SpaceStr, PureStr, LnBrkStr};
     const Str syntax2[6] = {PureStr, SpaceStr, DateStr, SpaceStr, DateStr, LnBrkStr};
 
-    const char **parameters[6];
+    char parameters[SNT_LIMIT][INP_LIMIT];
 
     TaskList targets;
     Task targetTask;
     if (parseParameters(str, syntax1, 4, parameters) == 1)
     {
-        const *id = parameters[2];
+        char *id = parameters[2];
         if (isEnrolledTask(id) == -1)
         {
             printSysMsg("해당하는 id의 일정이 없습니다.");
@@ -111,8 +136,8 @@ void del(const char *str)
 
     if (parseParameters(str, syntax2, 6, parameters) == 1)
     {
-        const *stDate = parameters[2];
-        const *enDate = parameters[4];
+        char *stDate = parameters[2];
+        char *enDate = parameters[4];
         if (dateCmpr(stDate, enDate) == -1)
         {
             printSysMsg("유효하지 않은 기간입니다.");
@@ -132,8 +157,8 @@ void del(const char *str)
     printSysMsg("위 일정을 삭제하시겠습니까?");
 
     char *uip;
-    const char **parameters2[2];
-    const char syntax3 = {BooleanStr, LnBrkStr};
+    char parameters2[SNT_LIMIT][INP_LIMIT];
+    const Str syntax3[2] = {BooleanStr, LnBrkStr};
     getUsrIpt(uip);
     if (strType(uip) == NotStr ||
         parseParameters(uip, syntax3, 2, parameters2) == -1 ||
@@ -154,7 +179,7 @@ void search(const char *str)
     const Str syntax1[2] = {PureStr, LnBrkStr};
     const Str syntax2[6] = {PureStr, SpaceStr, DateStr, SpaceStr, DateStr, LnBrkStr};
 
-    const char **parameters[6];
+    char parameters[SNT_LIMIT][INP_LIMIT];
 
     if (parseParameters(str, syntax1, 2, parameters) == 1)
     {
@@ -163,8 +188,8 @@ void search(const char *str)
 
     if (parseParameters(str, syntax2, 6, parameters) == 1)
     {
-        const *stDate = parameters[2];
-        const *enDate = parameters[4];
+        char *stDate = parameters[2];
+        char *enDate = parameters[4];
 
         if (dateCmpr(stDate, enDate) == -1)
         {
@@ -177,26 +202,3 @@ void search(const char *str)
 
     printSysMsg("문법에 맞지 않는 입력입니다.");
 }
-
-// 사용자 입력에서 인자를 파싱하고, 그럴 수 없으면 raise exception
-// 예시)
-// parseParameters("delete abc123\r\n", [PureStr, SpaceStr, PureStr, LnBrkStr], parameters);
-// parameters[0]에는 첫 번째 인자가 저장된다.
-int parseParameters(const char *usrIpt, const Str *cmdSyntax, const int syntaxLen, const char **parameters)
-{
-    int now = 0;
-    for (int i = 0; i < syntaxLen; i++)
-    {
-        int nxt = stepStr(usrIpt, now, cmdSyntax[i], parameters[i]);
-
-        if (nxt == 0)
-            return -1;
-
-        now += nxt;
-    }
-
-    if (strlen(usrIpt) > now)
-        return -1;
-
-    return 1;
-};
